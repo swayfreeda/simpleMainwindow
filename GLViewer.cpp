b4b3b643b4d7b071235b9e68d9e68e2ee3bfe202
swayfreeda <weisui021@gmail.com>
2014-11-07 11:22:21 +0800

    add a Qt version

--- GLViewer.cpp
+++ GLViewer.cpp
@@ -1,7 +1,13 @@
+
+
 #include<GL/glew.h>
 #include<GL/gl.h>
 #include<GL/glu.h>
 #include<GL/freeglut.h>
+
+#ifdef QT_BUFFER
+#include<QGLFunctions>
+#endif
 
 #include"GLViewer.h"
 #include <string>
@@ -21,6 +27,7 @@
 #include<QApplication> // qaApp
 #include<QTextStream>
 
+
 #include<time.h>
 #include<sstream>
 #include<cassert>
@@ -39,7 +46,7 @@ GLuint numElement;
 
 SW::GLViewer::GLViewer(QWidget *parent0, const QGLWidget *parent1, Qt::WFlags f): QGLViewer(parent0, parent1, f)
 {
-   setAutoFillBackground(true);
+    setAutoFillBackground(true);
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
 SW::GLViewer::~GLViewer()
@@ -91,11 +98,27 @@ void SW::GLViewer::init()
 
     // glew init is very important or errors occur
     glewInit();
-
     // generate vertex arrays, and each array is corresponding to a object to be render
     glGenVertexArrays(1, &arrayId);
 
+#ifdef QT_BUFFER
+    QGLFunctions qtgl;
+    qtgl.initializeGLFunctions(0);
+#endif
+
     // 3D world coordinate of points
+#ifdef QT_BUFFER
+      QVector<QVector3D> Verts;
+    Verts.append(QVector3D(-1.0, -1.0, -1.0));
+    Verts.append(QVector3D(-1.0, -1.0, 1.0));
+    Verts.append(QVector3D(-1.0, 1.0, -1.0));
+    Verts.append(QVector3D(-1.0, 1.0, 1.0));
+    Verts.append(QVector3D(1.0, -1.0, -1.0));
+    Verts.append(QVector3D(1.0, -1.0, 1.0));
+    Verts.append(QVector3D(1.0, 1.0, -1.0));
+    Verts.append(QVector3D(1.0, 1.0, 1.0));
+#else
+
     GLfloat Verts[][3] = {
         {-1.0, -1.0, -1.0},
         {-1.0, -1.0, 1.0},
@@ -106,8 +129,20 @@ void SW::GLViewer::init()
         {1.0, 1.0, -1.0},
         {1.0, 1.0, 1.0},
     };
+#endif
 
     // colors of points
+#ifdef QT_BUFFER
+     QVector<QVector3D> Colors;
+    Colors.append(QVector3D(0, 0.0, 0.0));
+    Colors.append(QVector3D(0, 0.0, 1.0));
+    Colors.append(QVector3D(0, 1.0, 0.0));
+    Colors.append(QVector3D(0, 1.0, 1.0));
+    Colors.append(QVector3D(1.0, 0.0, 0.0));
+    Colors.append(QVector3D(1.0, 0.0, 1.0));
+    Colors.append(QVector3D(1.0, 1.0, 0.0));
+    Colors.append(QVector3D(1.0, 1.0, 1.0));
+#else
     GLfloat Colors[][3] = {
         {0.0, 0.0, 0.0},
         {0.0, 0.0, 1.0},
@@ -118,8 +153,18 @@ void SW::GLViewer::init()
         {1.0, 1.0, 0.0},
         {1.0, 1.0, 1.0},
     };
+#endif
 
     // indices of points
+#ifdef QT_BUFFER
+    QVector<uint> Indices;
+    Indices.append(0);Indices.append(1);Indices.append(3);Indices.append(2);
+    Indices.append(4);Indices.append(6);Indices.append(7);Indices.append(5);
+    Indices.append(2);Indices.append(3);Indices.append(7);Indices.append(6);
+    Indices.append(0);Indices.append(4);Indices.append(5);Indices.append(1);
+    Indices.append(0);Indices.append(2);Indices.append(6);Indices.append(4);
+    Indices.append(1);Indices.append(5);Indices.append(7);Indices.append(3);
+#else
     GLubyte Indices[]={
         0, 1, 3, 2,
         4, 6, 7, 5,
@@ -128,31 +173,71 @@ void SW::GLViewer::init()
         0, 2, 6, 4,
         1, 5, 7, 3,
     };
+#endif
 
     // active a vertex array
     glBindVertexArray(arrayId);
 
     // generate buffer objects, and each attribute(vertices, color, and normal..) is corresponding to one buffer
-    glGenBuffers(NumVBOs, buffers);
 
+#ifdef QT_BUFFER
+    qtgl.glGenBuffers(NumVBOs, buffers);
+#else
+    glGenBuffers(NumVBOs, buffers);
+#endif
+
+    //---------------------------------------buffer for vertices----------------------------------//
     // active a buffer object
+#ifdef QT_BUFFER
+    qtgl.glBindBuffer(GL_ARRAY_BUFFER, buffers[Vertices]);
+#else
     glBindBuffer(GL_ARRAY_BUFFER, buffers[Vertices]);
+#endif
     // alloc a space for buffer
+#ifdef QT_BUFFER
+    qtgl.glBufferData(GL_ARRAY_BUFFER, Verts.size()*sizeof(GLfloat), Verts.data(), GL_STATIC_DRAW);
+#else
     glBufferData(GL_ARRAY_BUFFER, sizeof(Verts), Verts, GL_STATIC_DRAW);
+#endif
     // put the data into the corresponding buffer
-    glVertexPointer(3, GL_FLOAT, 0, BUFFER_OFFSET(0));
+    glVertexPointer(3, GL_FLOAT, 0,BUFFER_OFFSET(0));
     glEnableClientState(GL_VERTEX_ARRAY);
 
+
+
+    //---------------------------------------buffer for colors----------------------------------//
     // buffer for colors
+#ifdef QT_BUFFER
+    qtgl.glBindBuffer(GL_ARRAY_BUFFER, buffers[Color]);
+#else
     glBindBuffer(GL_ARRAY_BUFFER, buffers[Color]);
+#endif
+#ifdef QT_BUFFER
+    qtgl.glBufferData(GL_ARRAY_BUFFER, Colors.size()*sizeof(GLfloat), Colors.data(), GL_STATIC_DRAW);
+#else
     glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
+#endif
     glColorPointer(3, GL_FLOAT, 0, BUFFER_OFFSET(0));
     glEnableClientState(GL_COLOR_ARRAY);
 
+
+    //---------------------------------------buffer for elements----------------------------------//
     // buffer for elements
+#ifdef QT_BUFFER
+    numElement = Indices.size();
+#else
     numElement = sizeof(Indices)/sizeof(Indices[0]);
+#endif
+#ifdef QT_BUFFER
+    qtgl.glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[Elements]);
+#else
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[Elements]);
+#endif
+#ifdef QT_BUFFER
+    qtgl.glBufferData(GL_ELEMENT_ARRAY_BUFFER, Indices.size()*sizeof(uint), Indices.data(), GL_STATIC_DRAW);
+#else
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+#endif
 
 
 }
@@ -171,7 +256,12 @@ void SW::GLViewer::draw()
 
     // drawelements
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+#ifdef QT_BUFFER
+    glDrawElements(GL_QUADS, numElement,  GL_UNSIGNED_INT, BUFFER_OFFSET(0));
+#else
     glDrawElements(GL_QUADS, numElement,  GL_UNSIGNED_BYTE, BUFFER_OFFSET(0));
+#endif
 
     glFlush();
 }
@@ -223,31 +313,31 @@ void SW::GLViewer::drawAxises(double width, double length)
 
     //qglColor(Qt::green);
     glColor3f(0.0, 1.0, 0.0);
-   // renderText(0.0, axisLength, 0.0, "Y", QFont("helvetica", 12, QFont::Bold, TRUE));
+    // renderText(0.0, axisLength, 0.0, "Y", QFont("helvetica", 12, QFont::Bold, TRUE));
 
     //qglColor(Qt::blue);
     glColor3f(0.0, 0.0, 1.0);
-   // renderText(0.0, 0.0, axisLength, "Z", QFont("helvetica", 12, QFont::Bold, TRUE));
+    // renderText(0.0, 0.0, axisLength, "Z", QFont("helvetica", 12, QFont::Bold, TRUE));
 
 }
 
- void SW::GLViewer::mousePressEvent(QMouseEvent *e)
- {
+void SW::GLViewer::mousePressEvent(QMouseEvent *e)
+{
     QGLViewer::mousePressEvent(e);
- }
- void SW::GLViewer::mouseReleaseEvent(QMouseEvent *e)
- {
+}
+void SW::GLViewer::mouseReleaseEvent(QMouseEvent *e)
+{
     QGLViewer::mouseReleaseEvent(e);
- }
- void SW::GLViewer::mouseMoveEvent(QMouseEvent *e)
- {
+}
+void SW::GLViewer::mouseMoveEvent(QMouseEvent *e)
+{
     QGLViewer::mouseMoveEvent(e);
- }
- void SW::GLViewer::wheelEvent(QWheelEvent *e)
- {
-     QGLViewer::wheelEvent(e);
- }
- void SW::GLViewer::keyPressEvent(QKeyEvent *e)
- {
-     QGLViewer::keyPressEvent(e);
- }
+}
+void SW::GLViewer::wheelEvent(QWheelEvent *e)
+{
+    QGLViewer::wheelEvent(e);
+}
+void SW::GLViewer::keyPressEvent(QKeyEvent *e)
+{
+    QGLViewer::keyPressEvent(e);
+}
